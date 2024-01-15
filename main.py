7c1cfde96b1d2e28d93bf2a8d254a4d6e5b7e43d
sherkhan <sherkhan1167746@gmail.com>
2024-01-15 15:20:26 +0500

    add lines

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ pdf.set_auto_page_break(auto=False)
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
+    # set the master
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+    for y in range(0, 277, 10):
+        pdf.line(x1=10, y1=21 + y, x2=200, y2=21 + y)
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -18,8 +20,12 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(0,277, 10):
+            pdf.line(x1=10, y1=21 + y, x2=200, y2=21 + y)
+        # set the footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+
 pdf.output("output.pdf")
